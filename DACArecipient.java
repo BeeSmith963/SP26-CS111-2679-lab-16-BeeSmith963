641a7af83b50a2b843bdd2d1b79a1f9af9e7d641
Bee Smith <240beeline@gmail.com>
2026-05-13 01:48:56 +0000

    DONE

--- DACArecipient.java
+++ DACArecipient.java
@@ -16,7 +16,7 @@
 - uscisNumber : String
 - countryOfOrigin : String
 - birthday : int
-- validFormDate  : int
+- validFromDate  : int
 - expirationDate : int
 - sex : char
 -----------------------------------------
@@ -25,7 +25,7 @@
 + getUscisNumber() : String
 + getCountryOfOrigin() : String
 + getBirthday() : int
-+ getValidFormDate() : int
++ getValidFromDate() : int
 + getExpirationDate() : int
 + getSex(sex : char) : char
 
@@ -34,11 +34,11 @@
 + setUscisNumber(uscisNumber : String) : void
 + setCountryOfOrigin(countryOfOrigin : String) : void
 + setBirthday(birthday : int) : void
-+ setValidFormDate(validFormDate  : int) : void
++ setValidFromDate(validFromDate  : int) : void
 + setExpirationDate(expirationDate : int) : void
-+ setSex() : void
++ setSex(sex : char) : void
 
-+setAll(surname : String, givenName : String, uscisNumber : String, countryOfOrigin : String, birthday : int, validFormDate  : int, expirationDate : int, sex : char) : void
++setAll(surname : String, givenName : String, uscisNumber : String, countryOfOrigin : String, birthday : int, validFromDate  : int, expirationDate : int, sex : char) : void
 -----------------------------------------
 */
 
@@ -46,11 +46,11 @@ public class DACArecipient
 {
 	/***** INSTANCE VARIABLES *****/
 	//TODO: Declare the instance variables. 
-	public String surname;
+	private String surname;
 	private String givenName;
 	private String uscisNumber;
 	private String countryOfOrigin;
-	private int birthday, validFormDate, expirationDate;
+	private int birthday, validFromDate, expirationDate;
 	private char sex;
 
 	/***** ACCESSORS *****/
@@ -61,7 +61,8 @@ public class DACArecipient
 	 * 
 	 * @return String representing surname
 	 */
-	public String getSurname(){
+	public String getSurname()
+	{
 		return this.surname;
 	}
 	public String getGivenName()
@@ -74,15 +75,15 @@ public class DACArecipient
 	}
 	public String getCountryOfOrigin()
 	{
-		retunr this.countryOfOrigin;
+		return this.countryOfOrigin;
 	}
-	public int getBirthday;
+	public int getBirthday()
 	{
 		return this.birthday;
 	}
-	public int getValidFormDate()
+	public int getValidFromDate()
 	{
-		return this.validFormDate;
+		return this.validFromDate;
 	}
 	public int getExpirationDate()
 	{
@@ -94,8 +95,72 @@ public class DACArecipient
 	}
 
 	/***** MUTATORS *****/
-	//TODO: Write the setter for each instance variable. Remember to include documentation for each method.
+	//TODO: Write the setter for each instance variable. Remember to include
+	//  documentation for each method.
+	/**
+	 * Sets surname to parameter value of calling object.
+	 * 
+	 * @param surName String representing last name, assume it is valid
+	 */
+	public void setSurname(String surname)
+	{
+		this.surname = surname;
+	}
+	public void setGivenName(String givenName)
+	{
+		this.givenName = givenName;
+	}
+	public void setUscisNumber(String uscisNumber)
+	{
+		this.uscisNumber = uscisNumber;
+	}
+	public void setCountryOfOrigin(String countryOfOrigin)
+	{
+		this.countryOfOrigin = countryOfOrigin;
+	}
+	public void setBirthday(int birthday)
+	{
+		this.birthday = birthday;
+	}
+	public void setValidFromDate(int validFromDate)
+	{
+		this.validFromDate = validFromDate;
+	}
+	public void setExpirationDate(int expirationDate)
+	{
+		this.expirationDate = expirationDate;
+	}
+	public void setSex(char sex)
+	{
+		this.sex = sex;
+	}
 
 	//TODO: Write the setAll method. Remember to include documentation.
+	//+setAll(surname : String, givenName : String, uscisNumber : String, countryOfOrigin : String, birthday : int, validFromDate  : int, expirationDate : int, sex : char) : void
+	/**
+	 * sets all data for object
+	 * 
+	 * @Param surName String representing last name, assume it is valid
+	 * @Param givenName String representing first name, assume it is valid
+	 * @Param uscisNumber String representing 3-part USCIS number w dashes
+	 * @Param countryOfOrigin country name assume valid country 
+	 * @Param birthday Julian day number for birthday
+	 * @Param validFromDate Julian day number for valid From date
+	 * @Param expirationDate Julian day number for expiration date
+	 * @Param sex char representing sex
+	 * 
+	 */
 
+	public void setAll(String surname, String givenName,String uscisNumber, String countryOfOrigin, int birthday, int validFromDate, int expirationDate, char sex)
+	{
+		//this.surname = surname;
+		setSurname(surname);
+		setGivenName(givenName);
+		setUscisNumber(uscisNumber);
+		setCountryOfOrigin(countryOfOrigin);
+		setBirthday(birthday);
+		setValidFromDate(validFromDate);
+		setExpirationDate(expirationDate);
+		setSex(sex);
+	}
 }
--- Main.java
+++ Main.java
@@ -22,19 +22,45 @@ class Main
     // String givenName;
     // String uscisNumber;
     // String countryOfOrigin;
-    // int birthday, validFormDate, expirationDate;
+    // int birthday, validFromDate, expirationDate;
     // char sex;
 
-    DACArecipient test = new DACArecipient();
+    DACArecipient test1 = new DACArecipient();
     DACArecipient test2 = new DACArecipient();
-    DACArecipient test3 = new DACArecipient();
 
-    //test.surname = "Bee";
-    System.out.println("Test surname = " + test.getSurname());
-    System.out.println("Test birthday = " + test.getBirthday());
-    System.out.println("Test sex = " + test.getSex());
-
+    test1.setSurname("Smith");
+    test1.setGivenName("Bee");
+    test1.setUscisNumber("12-3-456");
+    test1.setCountryOfOrigin("USA");
+    test1.setBirthday(123456);
+    test1.setValidFromDate(789101);
+    test1.setExpirationDate(121314);
+    test1.setSex('F');
     
+
+    System.out.println("Person 1");
+    System.out.println("-------------------------------");
+    System.out.println("Surname: " + test1.getSurname());
+    System.out.println("Given Name: " + test1.getGivenName());
+    System.out.println("USCIS Number: " + test1.getUscisNumber());
+    System.out.println("Country of Origin: " + test1.getCountryOfOrigin());
+    System.out.println("Birthday (JDN): " + test1.getBirthday());
+    System.out.println("Valid From Date (JDM): " + test1.getValidFromDate());
+    System.out.println("Expiration Date (JDM): " + test1.getExpirationDate());
+    System.out.println("Sex: " + test1.getSex());
+    
+    test2.setAll("Mendez", "Javier", "56-3-445", "Venezuela", 2451564, 3956753, 3956840, 'M');
+
+    System.out.println("\n\nPerson 2");
+    System.out.println("-------------------------------");
+    System.out.println("Surname: " + test2.getSurname());
+    System.out.println("Given Name: " + test2.getGivenName());
+    System.out.println("USCIS Number: " + test2.getUscisNumber());
+    System.out.println("Country of Origin: " + test2.getCountryOfOrigin());
+    System.out.println("Birthday (JDN): " + test2.getBirthday());
+    System.out.println("Valid From Date (JDM): " + test2.getValidFromDate());
+    System.out.println("Expiration Date (JDM): " + test2.getExpirationDate());
+    System.out.println("Sex: " + test2.getSex());
 
   }
 }
